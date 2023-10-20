0afa7913ff1fff2f6f485c7f08d63174cf157871
soyUnaGata <fallen.snitch@gmail.com>
2023-10-20 09:26:14 +0300

    implement header template

---
+++ src/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+     <div class="container">
+        <div class="header__inner d-flex justify-content-between align-items-center">
+            <a class="logo" href="">
+                <img src="../assets/img/logo.svg" alt="logo">
+            </a>
+
+            <nav class="menu">
+                <ul class="menu__list d-flex align-items-center">
+                    <li class="menu__list-item">
+                        <a class="menu__list-link" href="">Homepage</a>
+                    </li>
+                    <li class="menu__list-item">
+                        <a class="menu__list-link" href="">Reciepe Page</a>
+                    </li>
+                    <li class="menu__list-item">
+                        <a class="menu__list-link" href="">Pages</a>
+                    </li>
+                    <li class="menu__list-item">
+                        <a class="menu__list-link" href="">Buy</a>
+                    </li>
+                </ul>
+            </nav>
+
+            <div class="search d-flex">
+               <div class="search__input">
+                    <img src="" alt="">
+                    <label for=""></label>
+                    <input type="text">
+               </div>
+
+                <li class="profile">
+                    <a class="profile-link" href="">
+                      
+                    </a>
+                </li>
+
+             
+
+               <div class="burger-menu__mobile">
+
+                <div class="burger-menu__about">
+          
+                </div>
+            </div>
+            </div>
+
+        </div>
+    </div>
+</template>
