47908f0556ffdc098c350c88e534da848f621ffb
soyUnaGata <fallen.snitch@gmail.com>
2023-10-24 11:18:42 +0300

    amend header

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,61 @@
 <template>
-  <Header></Header>
+     <div class="container d-flex">
+        <div class="header__inner d-flex justify-content-between align-items-center">
+            <a class="logo" href="">
+                <img src="./assets/img/logo.svg" alt="logo">
+            </a>
+
+            <nav class="menu">
+                <ul class="menu__list d-flex align-items-center">
+                    <li class="menu__list-item">
+                        <a class="menu__list-link" href="">Homepage</a>
+                    </li>
+                    <li class="menu__list-item">
+                        <a class="menu__list-link" href="">Reciepe Page</a>
+                    </li>
+                    <li class="menu__list-item">
+                        <a class="menu__list-link" href="">Pages</a>
+                    </li>
+                    <li class="menu__list-item">
+                        <a class="menu__list-link" href="">Buy</a>
+                    </li>
+                </ul>
+            </nav>
+
+            <div class="search d-flex">
+               <div class="search__input">
+                    <img src="./assets/img/search.svg" alt="">
+                    <label for="">
+                      <input type="text">
+                    </label>
+                    
+               </div>
+
+                <li class="profile">
+                    <a class="profile-link" href="">
+                      
+                    </a>
+                </li>
+
+             
+
+               <div class="burger-menu__mobile">
+
+                <div class="burger-menu__about">
+          
+                </div>
+            </div>
+            </div>
+
+        </div>
+    </div>
 </template>
 
 <script>
-import Header from './components/Header.vue';
+
   export default {
     components: {
-      Header
+     
     },
   }
 </script>
