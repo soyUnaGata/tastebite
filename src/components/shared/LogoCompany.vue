734725967350b1866444be03c789df91de7bf37b
soyUnaGata <fallen.snitch@gmail.com>
2023-11-15 16:31:57 +0300

    made logog component

---
+++ src/components/shared/LogoCompany.vue
@@ -0,0 +1,30 @@
+<template>
+  <a class="logo" href="">
+    <div class="logo__block d-flex align-items-center">
+      <img src="@/assets/img/logo.svg" class="logo__name" alt="" />
+      <svg
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        class="logo__dot"
+        viewBox="0 0 1024 1024"
+      >
+        <title />
+        <g id="icomoon-ignore" />
+        <path
+          d="M16 512c0 273.932 222.068 496 496 496s496-222.068 496-496v0c0-273.932-222.068-496-496-496s-496 222.068-496 496v0z"
+        />
+      </svg>
+    </div>
+  </a>
+</template>
+
+<style>
+.logo__name {
+  max-width: 150px;
+}
+
+.logo__dot {
+  height: 10px;
+  fill: #ff642f;
+}
+</style>
