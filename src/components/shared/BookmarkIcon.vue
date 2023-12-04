d76797f648fc30ec662c8b83ebf084230b96f60c
soyUnaGata <fallen.snitch@gmail.com>
2023-12-04 13:01:28 +0200

    implement bookmar component

---
+++ src/components/shared/BookmarkIcon.vue
@@ -0,0 +1,14 @@
+<template>
+    <svg class="bookmark__item d-flex align-items-center" version="1.1" xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 1024 1024">
+        <title></title>
+        <g id="icomoon-ignore">
+        </g>
+        <path d="M113.778 1023.999v-910.221q0-46.933 33.423-80.356t80.356-33.423h568.889q46.933 0 80.356 33.423t33.423 80.356v910.221l-398.222-170.666-398.222 170.666zM227.556 850.489l284.444-122.311 284.444 122.311v-736.71h-568.889v736.71zM227.556 113.778v0z"></path>
+    </svg>
+</template>
+
+<style scoped>
+.bookmark__item {
+    cursor: pointer;
+}
+</style>
