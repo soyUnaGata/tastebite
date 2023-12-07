0e8beb35991ecfcf20288856bdf55158aa150f55
soyUnaGata <fallen.snitch@gmail.com>
2023-12-07 11:57:25 +0200

    separate profile-section

---
+++ src/components/shared/ProfileSection.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="profile__section">
+        <li class="profile">
+            <a class="profile-link" href="">
+                <img class="account__icon" src="@/assets/img/account.svg" alt="" />
+            </a>
+        </li>
+
+        <div class="burger-menu__mobile d-none">
+            <div class="burger-menu__about"></div>
+        </div>
+    </div>
+</template>
