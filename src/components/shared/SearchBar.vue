c5956694b94435dc259d7f1b2ec493666ed5511f
soyUnaGata <fallen.snitch@gmail.com>
2023-12-09 17:04:33 +0200

    implement cancel btn

--- src/components/shared/SearchBar.vue
+++ src/components/shared/SearchBar.vue
@@ -5,9 +5,17 @@
         <Transition name="slide-fade">
           <label class="search__input" v-if="searchInputState" for="">
               <input class="search__input-item" 
+              v-model="query"
               placeholder="Search recipe..."
               @input="search" type="text" 
               @keyup.enter="showFullList" />
+              <button class="clean__search" type="button"
+              v-show="query.length"
+              @click="cleanSearch">
+                <svg class="clean__search-svg" xmlns="http://www.w3.org/2000/svg" height="25" viewBox="0 -960 960 960" width="25">
+                  <path d="m336-280 144-144 144 144 56-56-144-144 144-144-56-56-144 144-144-144-56 56 144 144-144 144 56 56ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/>
+                </svg>
+              </button>
           </label>
         </Transition>
           <img class="search__icon" @click="showSearchInput" src="@/assets/img/search.svg" alt="" />
@@ -37,15 +45,18 @@ export default ({
       meals: [],
     }
   },
-  watch: {
-    // 'query': function () {
-    //   if (!this.searchInputState){
-    //     this.query = '';
-    //     console.log(this.query)
-    //   }
-    // console.log(this.query)
-    // }
-  },
+  // watch: {
+  //   'query': function () {
+  //    if(){
+  //     this.cleanSearch()
+  //    }
+  //     // if (!this.searchInputState){
+  //     //   this.query = '';
+  //     //   console.log(this.query)
+  //     // }
+  //   // console.log(this.query)
+  //   }
+  // },
   computed: {
   },
   methods: {
@@ -57,7 +68,6 @@ export default ({
     },
 
     search: debounce(async function (e) {
-      this.query = e.target.value;
       this.meals = await SearchService.search(this.query);
       if (e.keyCode === 13) {
         this.query = ''
@@ -75,6 +85,11 @@ export default ({
       this.searchInputState = false;
       this.query = '';
       this.$emit('show-full-recipe', meal);
+    },
+
+    cleanSearch(){
+
+      this.query = '';
     }
   },
   async mounted() {
@@ -100,6 +115,25 @@ export default ({
 
 .search {
   gap: 15px;
+}
+
+.search__input {
+  position: relative;
+}
+
+.clean__search {
+position: absolute;
+  right: 0;
+  border-radius: 50%;
+  transform: translate(-50%, 34%);
+  border: none;
+  outline: none;
+  background: none;
+  cursor: pointer;
+}
+
+.clean__search-svg {
+  fill: var(--danger-color);
 }
 .search__input-item {
   height: 45px;
@@ -180,12 +214,9 @@ input[type="text"].search__input-item::placeholder {
   overflow-y: scroll;
   z-index: 89;
   position: absolute;
-  /* background: var(--background-elements); */
   background: var(--white);
   border-radius: 10px;
   box-shadow: 0 4px 30px rgb(0 0 0 / 10%);
-  /* backdrop-filter: blur(3.7px); */
-  /* -webkit-backdrop-filter: blur(3.7px); */
   border: 1px solid rgba(255, 100, 47, 0.67);
   padding-left: 7px;
   padding-top: 10px;
