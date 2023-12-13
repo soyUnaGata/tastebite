ec2fd5530b67023d656e23dd059810833d60b323
soyUnaGata <fallen.snitch@gmail.com>
2023-12-13 17:07:25 +0200

    implement router to main page

--- src/components/shared/PageHeader.vue
+++ src/components/shared/PageHeader.vue
@@ -1,42 +1,44 @@
 <template>
     <div class="d-flex w-100">
         <div class="header__inner d-flex align-items-center">
-            <logo-company/>
+            <a :href="'/'">
+                <logo-company />
+            </a>
 
             <nav class="menu">
                 <ul class="menu__list d-flex align-items-center">
-                <li class="menu__list-item">
-                    <a class="menu__list-link" href="">Homepage</a>
-                </li>
-                <li class="menu__list-item">
-                    <a class="menu__list-link" href="">Reciepe Page</a>
-                </li>
-                <li class="menu__list-item">
-                    <a class="menu__list-link" href="">Pages</a>
-                </li>
-                <li class="menu__list-item">
-                    <a class="menu__list-link" href="">Buy</a>
-                </li>
+                    <li class="menu__list-item">
+                        <a class="menu__list-link" :href="'/'">Homepage</a>
+                    </li>
+                    <li class="menu__list-item">
+                        <a class="menu__list-link" href="">Reciepe Page</a>
+                    </li>
+                    <li class="menu__list-item">
+                        <a class="menu__list-link" href="">Pages</a>
+                    </li>
+                    <!-- <li class="menu__list-item">
+                        <a class="menu__list-link" href="">Buy</a>
+                    </li> -->
                 </ul>
-            </nav>              
+            </nav>
         </div>
-  </div>
+    </div>
 </template>
 
 <script>
-    import LogoCompany from './LogoCompany.vue';
+import LogoCompany from './LogoCompany.vue';
 
-    export default ({
-        components: {
-            LogoCompany,
-        }
-    })
+export default ({
+    components: {
+        LogoCompany,
+    }
+})
 </script>
 
 <style>
 .container {
-  max-width: 1240px;
-  margin: 0 auto;
+    max-width: 1240px;
+    margin: 0 auto;
 }
 
 .menu {
@@ -44,18 +46,17 @@
 }
 
 .header__inner {
-  padding: 30px 0px;
-  width: 100%;
+    padding: 30px 0px;
+    width: 100%;
 }
 
 .menu__list-link {
-  font-size: 18px;
-  color: var(--primary);
-  font-weight: 600;
+    font-size: 18px;
+    color: var(--primary);
+    font-weight: 600;
 }
 
 .menu__list {
-  gap: 30px;
+    gap: 30px;
 }
-
 </style>
--- src/pages/MealsListFavorite.vue
+++ src/pages/MealsListFavorite.vue
@@ -62,7 +62,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .nav__about {
     gap: 20px;
 }
