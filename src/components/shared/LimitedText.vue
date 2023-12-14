4e2a87f2f7cef429d8b43a794e5670a54b0eb07d
soyUnaGata <fallen.snitch@gmail.com>
2023-12-14 16:27:53 +0200

    implement text-limit

---
+++ src/components/shared/LimitedText.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="description__part">
+        <span class="description">{{ displayedText }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LimitedText',
+    props: {
+        text: {
+            type: String,
+            required: true
+        },
+        limit: {
+            type: Number,
+            default: 210
+        }
+    },
+    data() {
+        return {
+            limited: true
+        }
+    },
+    computed: {
+        overflowLimit() {
+            return this.text.length > this.limit;
+        },
+        displayedText() {
+            if (!this.overflowLimit || !this.limited) return this.text;
+            return this.text.substring(0, this.limit - 3) + '...';
+        }
+    },
+}
+</script>
+
+<style>
+.description {
+    font-size: 16px;
+    font-weight: 400;
+}
+
+.description>span {
+    cursor: pointer;
+}
+</style>
