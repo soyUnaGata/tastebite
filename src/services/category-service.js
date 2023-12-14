e3cb0ba0ed9a6b0786d74a58e836836c1386cf3a
soyUnaGata <fallen.snitch@gmail.com>
2023-12-14 12:51:58 +0200

    implement category service

--- src/services/category-service.js
+++ src/services/category-service.js
@@ -1,8 +1,19 @@
-import api from './api';
+import api from "./api";
 
 class CategoryService {
   async getAll() {
-    return await api.get(`categories.php`).then((response) => response.data);
+    return await api
+      .get("categories.php")
+      .then((response) => response.data)
+      .then((response) => {
+        if (!response.categories) return [];
+        return response.categories.map((category) => ({
+          id: category.idCategory,
+          category: category.strCategory,
+          description: category.strCategoryDescription,
+          img: category.strCategoryThumb,
+        }));
+      });
   }
 }
 
